765eeb3707a8e6122b303962fef7355f627d357a
ahaid <ahaidahaid@>
2023-04-06 15:14:23 -0500

    would_you_rather_1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+    <h1> Would you rather...</h1>
+
+    <would-you-rather-1 
+    v-bind:question="wyrQuestion"
+    v-bind:answer1="wyrAnswer1"
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-changed="answerChanged">
+    </would-you-rather-1>
+
+    <p>{{ userSelectionMessage }}</p>
+
+  </div>
+
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather1 from './components/WouldYouRather1.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather1
+  },
+  data() {
+    return {
+      wyrQuestion: 'Would you rather live in a place with a lot of trees or live in a place near the ocean?',
+      wyrAnswer1: 'Live in a place with a lot of trees',
+      wyrAnswer2: 'Live in a place near the ocean',
+      userSelectionMessage: ''
+    }
+  },
+  methods: {
+    answerChanged() {
+      this.userSelectionMessage= 'Thanks! you chose ${choice}'
+    }
   }
 }
 </script>
 
 <style>
+
+body {
+  background: gray;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +53,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background: peachpuff;
 }
 </style>
